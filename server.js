caebdfe6b2c68d99e51c00624b34e3d8a7b91c2a
adam <bcuz@users.noreply.github.com>
2019-05-13 23:45:18 -0400

    use routers

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express'); // importing a CommonJS module
 
+const postRouter = require('./posts/postRouter.js');
+const userRouter = require('./users/userRouter.js');
+
 const server = express();
 
 server.use(logger)
+
+server.use('/posts', postRouter);
+server.use('/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
