b937a7a32de1ca30445ac2ca3cafe6d58743db39
adam <bcuz@users.noreply.github.com>
2019-05-13 23:37:28 -0400

    custom logger

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express'); // importing a CommonJS module
 
 const server = express();
 
+server.use(logger)
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
@@ -9,7 +11,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(`${req.method} ${req.url} ${new Date().toISOString()}`);
 
+  next()
 };
 
 module.exports = server;
