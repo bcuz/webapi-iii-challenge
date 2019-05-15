26bdb5a8d1180da8d26b44a579a2bb11d39daf83
adam <bcuz@users.noreply.github.com>
2019-05-14 22:30:44 -0400

    update, delete user

--- users/userRouter.js
+++ users/userRouter.js
@@ -82,12 +82,38 @@ router.get('/:id/posts', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+  try {
+    const count = await Users.remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: 'The user has been nuked' });
+    } else {
+      res.status(404).json({ message: 'The user could not be found' });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error removing the user',
+    });
+  }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', async (req, res) => {
+  try {
+    const user = await Users.update(req.params.id, req.body);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'The user could not be found' });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error updating the user',
+    });
+  }
 });
 
 //custom middleware
