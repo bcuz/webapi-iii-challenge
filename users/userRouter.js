203f94aca5450e3ea9be067cc7d4dba9a1703018
adam <bcuz@users.noreply.github.com>
2019-05-14 22:04:29 -0400

    get all users, get user by id

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');;
 
+const Users = require('./userDb.js');
+const Posts = require('../posts/postDb.js');
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -10,12 +13,35 @@ router.post('/:id/posts', (req, res) => {
 
 });
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.get();
+    res.status(200).json(users);
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the users',
+    });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await Users.getById(req.params.id);
 
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'user not found' });
+    }
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the user',
+    });
+  }
 });
 
 router.get('/:id/posts', (req, res) => {
